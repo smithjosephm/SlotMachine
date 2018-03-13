4321db1a511caa24b1c212e0508e5a1b07d24518
smithjosephm <36716873+smithjosephm@users.noreply.github.com>
2018-03-13 14:52:26 -0500

    Update slotmachine.py

--- slotmachine.py
+++ slotmachine.py
@@ -1,5 +1,6 @@
 print("Welcome to the Slot Machine!")
 numberOfTimes = input('How many times do you want to play?')
+creditsplayed = float(raw_input('How many credits would you like to bet?'))
 slotsPossible = ["bar","bar","bar","cherry","crown","crown"]
 from random import *
 def play():
@@ -8,11 +9,11 @@ def play():
     slot3=choice(slotsPossible)
     win = ""
     if (slot1==slot2==slot3=="cherry"):
-        win = "You win $100"
+        win = "\nYou win $%d, WooHoo" % (creditsplayed * 10)
     if (slot1==slot2==slot3=="crown"):
-        win = "You win $50"
+        win = "\nYou win $%d" % (creditsplayed * 5)
     if (slot1==slot2==slot3=="bar"):
-        win = "You win $5"
+        win = "\nYou win $%d, not too bad." % (creditsplayed * 2)
     return slot1+":"+slot2+":"+slot3+" "+win
 for i in range(int(numberOfTimes)):
     print(play())
